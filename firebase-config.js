a2fe4b430c0deaa650a155025ae939f0f6cba7eb
ao-magicianED <2520000530@campus.ouj.ac.jp>
2026-01-14 15:16:44 +0900

    Revert authDomain to firebaseapp.com - keep popup for iOS

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@
 
 const firebaseConfig = {
     apiKey: "AIzaSyBEnj3BBBDkT0pfdW6jLvJxJuC-b4FYNI0",
-    authDomain: "ao-magicianed.github.io",
+    authDomain: "aozu-ops-hub.firebaseapp.com",
     projectId: "aozu-ops-hub",
     storageBucket: "aozu-ops-hub.firebasestorage.app",
     messagingSenderId: "228102458914",
